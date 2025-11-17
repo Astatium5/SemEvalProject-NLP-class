a9f1c1976d8fd6bcca4f6ec25abca34e5adecf89
Sungboo Park <parksb08@naver.com>
2025-11-16 20:41:14 -0700

    add roberta considering rare case

---
+++ encoder_inference.py
@@ -0,0 +1,142 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.optim import AdamW
+from datasets import load_dataset
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import argparse
+import pandas as pd
+import os
+
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:24"
+
+# torch.cuda.empty_cache()
+
+device = torch.device(
+    "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+parser = argparse.ArgumentParser(description='')
+    
+# Adding arguments
+parser.add_argument('--model_name', type=str)
+parser.add_argument('--experiment', type=str)
+args = parser.parse_args()
+
+model_name = args.model_name
+experiment = args.experiment
+
+
+
+# Define your dataset class
+class CustomDataset(Dataset):
+    def __init__(self, texts, labels, max_length=512):  # You can set max_length to an appropriate value
+
+        self.max_length = max_length
+        self.texts = texts
+        self.labels = labels
+
+        
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        inputs = tokenizer(
+            self.texts[idx],
+            return_tensors='pt',
+            # truncation=True,
+            padding='max_length',  # Use padding to ensure all sequences have the same length
+            max_length=self.max_length
+        )
+
+        is_truncated = False
+        if len(inputs['input_ids'][0]) > self.max_length:
+            is_truncated = True
+
+        inputs = tokenizer(
+            self.texts[idx],
+            return_tensors='pt',
+            truncation=True,
+            padding='max_length',  # Use padding to ensure all sequences have the same length
+            max_length=self.max_length
+        )
+           
+        label = torch.tensor(self.labels[idx])
+        return inputs, label, is_truncated
+
+def collate_fn(batch):
+    inputs, labels, is_truncated = zip(*batch)
+    return {
+        'input_ids': torch.stack([x['input_ids'].squeeze() for x in inputs]),
+        'attention_mask': torch.stack([x['attention_mask'].squeeze() for x in inputs]),
+        'labels': torch.tensor(labels), 
+        'is_truncated': is_truncated
+    }
+
+
+if experiment == "evasion_based_clarity":
+    num_labels = 9
+    mapping_labels = {'Explicit': 0, 'Implicit': 1, 'Dodging': 2, 'Deflection': 3, 'Partial': 4, 'General': 5, 'Declining': 6, 'Ignorance': 7, 'Clarification': 8}
+    label = "evasion_label"
+elif experiment == "direct_clarity":
+    num_labels = 3
+    mapping_labels = {'Clear Reply': 0, "Ambivalent": 1, "Clear Non-Reply": 2}
+    label = "clarity_label"
+
+
+# Load pre-trained RoBERTa model and tokenizer
+if 'roberta' in model_name: 
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    model = RobertaForSequenceClassification.from_pretrained(f"{model_name}-qaevasion-{experiment}", num_labels=num_labels).to(device)
+    max_length = 512
+
+elif "xlnet" in model_name:
+    # Load pre-trained XLNet model and tokenizer
+    from transformers import XLNetTokenizer, XLNetForSequenceClassification, AdamW
+    tokenizer = XLNetTokenizer.from_pretrained(model_name)
+    model = XLNetForSequenceClassification.from_pretrained(f"{model_name}-qaevasion-{experiment}", num_labels=num_labels).to(device)
+    max_length = 4096
+    
+elif "deberta" in model_name: 
+    from transformers import DebertaTokenizer, DebertaForSequenceClassification, AdamW
+    tokenizer = DebertaTokenizer.from_pretrained(model_name)
+    model = DebertaForSequenceClassification.from_pretrained(f"{model_name.split('/')[-1]}-qaevasion-{experiment}", num_labels=num_labels).to(device)
+    max_length = 512
+
+
+dataset = load_dataset("ailsntua/QEvasion")
+all_texts = [f"Question: {row['interview_question']}\n\nAnswer: {row['interview_answer']}\n\nSubanswer: {row['question']}" for row in dataset['test']]
+all_labels = [mapping_labels[row[label]] for row in dataset['test']]
+
+# Create datasets and dataloaders
+val_dataset = CustomDataset(all_texts, all_labels, max_length=512)
+val_dataloader = DataLoader(val_dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+
+# Inside the validation loop
+import numpy as np
+
+model.eval()
+inv_mapping_labels = {v:k for k, v in mapping_labels.items()}
+results = []
+
+true_labels, pred_labels = [], []
+with torch.no_grad():
+    for batch in tqdm(val_dataloader):
+        inputs = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+        is_truncated = batch['is_truncated']
+        
+        outputs = model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
+        for true_label, pred_label, is_trunc in zip(labels.cpu().numpy(), outputs["logits"].cpu().numpy(), is_truncated):
+            true_label = inv_mapping_labels[true_label]
+            pred_label = inv_mapping_labels[np.argmax(pred_label)]
+            results.append([is_trunc, true_label, pred_label])
+
+df = pd.DataFrame(results, columns=['is_truncated', 'true_labels', 'pred_labels'])
+os.makedirs("./results", exist_ok=True)
+df.to_csv(f"./results/{model_name.split('/')[-1]}-{experiment}.csv")
+
---
+++ encoder_train.py
@@ -0,0 +1,204 @@
+import torch
+from torch.utils.data import DataLoader, Dataset, WeightedRandomSampler
+from torch.optim import AdamW
+from datasets import load_dataset
+from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, accuracy_score
+from tqdm import tqdm
+import argparse
+from collections import Counter
+
+
+# import os
+
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:24"
+
+import torch
+# torch.cuda.empty_cache()
+
+device = torch.device(
+    "mps" if torch.backends.mps.is_available()
+    else "cpu"
+)
+
+parser = argparse.ArgumentParser(description='')
+    
+# Adding arguments
+parser.add_argument('--model_name', type=str)
+parser.add_argument('--experiment', type=str)
+args = parser.parse_args()
+
+model_name = args.model_name
+experiment = args.experiment
+
+
+if experiment == "evasion_based_clarity":
+    num_labels = 9
+    mapping_labels = {'Explicit': 0, 'Implicit': 1, 'Dodging': 2, 'Deflection': 3, 'Partial': 4, 'General': 5, 'Declining': 6, 'Ignorance': 7, 'Clarification': 8}
+    label = "evasion_label"
+elif experiment == "direct_clarity":
+    num_labels = 3
+    mapping_labels = {'Clear Reply': 0, "Ambivalent": 1, "Clear Non-Reply": 2}
+    label = "clarity_label"
+
+
+# Load pre-trained RoBERTa model and tokenizer
+if 'roberta' in model_name: 
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+    model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels=num_labels).to(device)
+    max_size = 512
+
+elif "xlnet" in model_name:
+    # Load pre-trained XLNet model and tokenizer
+    from transformers import XLNetTokenizer, XLNetForSequenceClassification
+    tokenizer = XLNetTokenizer.from_pretrained(model_name)
+    model = XLNetForSequenceClassification.from_pretrained(model_name, num_labels=num_labels).to(device)
+    max_size = 4096
+
+elif "deberta" in model_name: 
+    from transformers import DebertaTokenizer, DebertaForSequenceClassification
+    tokenizer = DebertaTokenizer.from_pretrained(model_name)
+    model = DebertaForSequenceClassification.from_pretrained(model_name, num_labels=num_labels).to(device)
+    max_size = 512
+
+
+# Define your dataset class
+class CustomDataset(Dataset):
+    def __init__(self, texts, labels, tokenizer, max_length=512):
+        self.max_length = max_length
+        self.input_ids = []
+        self.attention_masks = []
+        self.labels = []
+
+        for text, label in zip(texts, labels):
+            enc = tokenizer(
+                text,
+                truncation=True,
+                padding="max_length",
+                max_length=max_length,
+                return_tensors="pt"
+            )
+
+            # enc["input_ids"]: (1, max_length) → squeeze(0) = (max_length)
+            self.input_ids.append(enc["input_ids"].squeeze(0))
+            self.attention_masks.append(enc["attention_mask"].squeeze(0))
+            self.labels.append(torch.tensor(label))
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": self.input_ids[idx],
+            "attention_mask": self.attention_masks[idx],
+            "labels": self.labels[idx],
+        }
+
+
+# Example data
+
+dataset = load_dataset("ailsntua/QEvasion")
+all_texts = [f"Question: {row['interview_question']}\n\nAnswer: {row['interview_answer']}\n\nSubanswer: {row['question']}" for row in dataset['train']]
+all_labels = [mapping_labels[row[label]] for row in dataset['train']]
+print (set(all_labels))
+print (len(all_texts))
+
+train_texts, val_texts, train_labels, val_labels = train_test_split(
+    all_texts,
+    all_labels,
+    test_size=0.1,      # 10% validation
+    random_state=42,    # reproducibility
+    stratify=all_labels # stratify by labels
+)
+
+# Create datasets and dataloaders
+train_dataset = CustomDataset(train_texts, train_labels, max_length=512, tokenizer=tokenizer)
+val_dataset = CustomDataset(val_texts, val_labels, max_length=512, tokenizer=tokenizer)
+
+label_counts = Counter(all_labels)
+print(label_counts)
+num_labels = len(mapping_labels)
+total = sum(label_counts.values())
+class_weights = torch.tensor(
+    [total / label_counts[i] for i in range(num_labels)], 
+    dtype=torch.float
+).to(device)
+
+loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
+
+print("Class weights:", class_weights)
+
+sample_weights = [1.0 / label_counts[label] for label in train_labels]
+sample_weights = torch.tensor(sample_weights, dtype=torch.float)
+
+# balancing batch sampler
+sampler = WeightedRandomSampler(
+    weights=sample_weights,
+    num_samples=len(sample_weights),
+    replacement=True
+)
+train_dataloader = DataLoader(train_dataset, batch_size=4, sampler=sampler)
+val_dataloader = DataLoader(val_dataset, batch_size=4)
+
+
+print (len(train_dataloader), len(val_dataloader))
+
+# Fine-tuning
+optimizer = AdamW(model.parameters(), lr=1e-5)
+
+num_epochs = 10
+for epoch in range(num_epochs):
+    # Training
+    model.train()
+    for batch in tqdm(train_dataloader, desc=f'Epoch {epoch + 1}/ {num_epochs} - Training'):
+        inputs = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['labels'].to(device)
+    
+        outputs = model(input_ids=inputs, attention_mask=attention_mask)
+        logits = outputs.logits
+
+        loss_fn = torch.nn.CrossEntropyLoss(weight=class_weights)
+        loss = loss_fn(logits, labels)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+        optimizer.zero_grad()
+
+    # Inside the validation loop
+    model.eval()
+    val_loss = 0.0
+    
+    pred_labels = [] 
+    true_labels = [] 
+    with torch.no_grad():
+        for batch in tqdm(val_dataloader, desc=f'Epoch {epoch + 1}/{num_epochs} - Validation'):
+            inputs = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
+    
+            outputs = model(input_ids=inputs, attention_mask=attention_mask, labels=labels)
+            loss = outputs.loss
+            val_loss += loss.item()
+    
+            # Calculate accuracy
+            logits = outputs.logits
+            _, predicted = torch.max(logits, 1)
+            pred_labels.extend(predicted.cpu().numpy())
+            true_labels.extend(labels.cpu().numpy())
+
+            pred_labels.extend(predicted.cpu().numpy())
+            true_labels.extend(labels.cpu().numpy())
+
+
+    average_val_loss = val_loss / len(val_dataloader)
+    accuracy = accuracy_score(true_labels, pred_labels)
+    macro_f1 = f1_score(true_labels, pred_labels, average='macro')
+
+    print(f'Epoch {epoch + 1}/{num_epochs} - Validation Loss: {average_val_loss:.4f} - Accuracy: {accuracy * 100:.2f}% - Macro F1 Score: {macro_f1:.4f}')
+
+# Save the fine-tuned model
+
+out_file = f"{model_name.split('/')[-1]}-qaevasion-{experiment}"
+model.save_pretrained(out_file)
